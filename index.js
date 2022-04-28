f7c7622e3a52d49059d43cb3b5aef6d58dab8956
ImDamianCortes <dcortesmunoz@gmail.com>
2022-04-28 13:33:50 -0400

    correccion de funcionalidad

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const {usuarios} = require('./db/usuarios.json');
+const { usuarios } = require('./db/usuarios.json');
 
 
 app.use(express.static('assets'));
@@ -15,29 +15,42 @@ app.get("/abracadabra/usuarios:", (req, res) => {
     res.sendFile(__dirname + '/db/usuarios.json');
 });
 
-const acceso = false;
+let acceso = false;
 
-app.get("/abracadabra/juego/:usuario", (req, res) => {
-    console.log(acceso);
+const liberarAcceso = async (res) => {
+    let acceso = true;
+    console.log(acceso)
+    res.redirect("/abracadabra")
+}
+
+app.get("/abracadabra/juego/:usuario", async (req, res) => {
+
     const usuario = req.params.usuario;
-    console.log(usuario);
-    console.log(usuarios);
-    usuarios.some
-
+    const exist = await usuarios.includes(usuario)
+    exist
+        ? liberarAcceso(res)
+        : res.sendFile(__dirname + '/assets/img/who.png')
+    /*
+    if (exist == true){
+        res.redirect("/abracadabra")
+    } else{
+        res.sendFile(__dirname + '/assets/img/who.png')
+    }
+    */
 });
 
-const nuemeroAleatorio = Math.floor(Math.random() * (5 - 1) + 1);
-
-app.get("/usuario/:nombre", (req, res) => {
-    const nombre = req.params.nombre;
-    const test = nombre.match(/^[aeiouAEIOU]/)
-    test
-        ? res.send("Si, tu nombre empieza con una vocal")
-        : res.send("No, tu nombre no empieza con una vocal");
+app.get("/abracadabra/conejo/:n", (req, res) => {
+    const n = req.params.n;
+    const nuemeroAleatorio = Math.floor(Math.random() * (5 - 1) + 1);
+    
+    if (n == nuemeroAleatorio) {
+        res.sendFile(__dirname + '/assets/img/conejito.png')
+    } else {
+        res.sendFile(__dirname + '/assets/img/voldemort.png')
+    }
 });
-
 
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000!\nhttp://localhost:3000/abracadabra');
+    console.log('Server started on port 3000!\nhttp://localhost:3000/abracadabra/juego');
 });
